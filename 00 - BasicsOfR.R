9c7d3b9a8411c54f340c1357e967628cf5753ae5
nrachero <142378544+NorielAchero@users.noreply.github.com>
2024-08-08 01:21:43 +0800

    Add files via upload

---
+++ 00 - BasicsOfR.R
@@ -0,0 +1,50 @@
+# Storing Value in a Data Type
+
+a = "mobile"
+a = "pen"
+a = "makeup"
+-----------
+num1 = 3
+char1 = "hello"
+log1 = TRUE
+complex1 = 4+7i
+-----------
+  
+# Assignment Operator
+b = 1
+b <- 1
+1 -> b
+-----------
+  
+#Arithmetic Operators
+
+num1 = 10
+num2 = 20
+
+num1+num2
+num1-num2
+num1*num2
+num1/num2
+----------
+  
+# Relational Operators
+  
+num1<num2
+num1>num2
+num1==num2
+num1!=num2
+
+# Logical Operators
+
+log1 = TRUE
+log2 = FALSE
+
+log1&log1 #TRUE
+log1&log2 #FALSE
+log2&log2 #FALSE
+
+log1|log1 #TRUE
+log1|log2 #TRUE
+log2|log2 #FALSE
+
+
